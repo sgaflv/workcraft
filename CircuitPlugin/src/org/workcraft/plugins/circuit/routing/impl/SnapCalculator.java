16245b6007ef8ac07e51c40431b55930657a224c
Stanislav Golubtsov <sgaflv@gmail.com>
2016-07-24 12:05:20 +0200

    snap to lower added

--- CircuitPlugin/src/org/workcraft/plugins/circuit/routing/impl/SnapCalculator.java
+++ CircuitPlugin/src/org/workcraft/plugins/circuit/routing/impl/SnapCalculator.java
@@ -5,7 +5,7 @@ import org.workcraft.plugins.circuit.routing.primitive.RoutingConstants;
 public class SnapCalculator {
 	
 	/**
-	 * Snap given value to a higher value
+	 * Snap given value to a higher value. The snapSize must be positive.
 	 * @param value value to snap
 	 * @param snapSize the size of the snap
 	 * @return snapped value
@@ -16,6 +16,20 @@ public class SnapCalculator {
 		double divided = (value-RoutingConstants.EPSILON)/snapSize;
 		double ceil = Math.ceil(divided);
 		return ceil*snapSize;
+	}	
+	
+	/**
+	 * Snap given value to a lower value. The snapSize must be positive.
+	 * @param value value to snap
+	 * @param snapSize the size of the snap
+	 * @return snapped value
+	 */
+	public static double snapToLower(double value, double snapSize) {
+		assert snapSize > 0 : "snapSize must be positive";
+		
+		double divided = (value+RoutingConstants.EPSILON)/snapSize;
+		double floor = Math.floor(divided);
+		return floor*snapSize;
 	}	
 	
 }
--- CircuitPlugin/test/org/workcraft/plugins/circuit/routing/impl/SnapCalculatorTest.java
+++ CircuitPlugin/test/org/workcraft/plugins/circuit/routing/impl/SnapCalculatorTest.java
@@ -31,6 +31,64 @@ public class SnapCalculatorTest {
 		assertEquals(10, SnapCalculator.snapToHigher(3.5, 10), RoutingConstants.EPSILON);
 		assertEquals(10, SnapCalculator.snapToHigher(2*RoutingConstants.EPSILON, 10), RoutingConstants.EPSILON);
 		assertEquals(20, SnapCalculator.snapToHigher(10.001, 10), RoutingConstants.EPSILON);
-		fail("test failure");
 	}
+
+	
+	@Test
+	public void testSnapToHigherNegative() {
+		assertEquals(-3.5, SnapCalculator.snapToHigher(-3.501, 0.5), RoutingConstants.EPSILON);
+		assertEquals(-3.0, SnapCalculator.snapToHigher(-3.409, 0.5), RoutingConstants.EPSILON);
+	}	
+	
+	@Test
+	public void testSnapToHigherNegativeRemaining() {
+		assertEquals(-3.5, SnapCalculator.snapToHigher(-3.5, 0.5), RoutingConstants.EPSILON);
+		assertEquals(-3.5, SnapCalculator.snapToHigher(-3.5+RoutingConstants.EPSILON, 0.5), RoutingConstants.EPSILON);
+		assertEquals(-3.5, SnapCalculator.snapToHigher(-3.5-RoutingConstants.EPSILON, 0.5), RoutingConstants.EPSILON);
+	}
+	
+
+	
+	
+
+	@Test
+	public void testSnapToLowerSimple() {
+		assertEquals(0.0, SnapCalculator.snapToLower(0.0, 1), RoutingConstants.EPSILON);
+		assertEquals(0.0, SnapCalculator.snapToLower(0.1, 1), RoutingConstants.EPSILON);
+	}
+
+	@Test
+	public void testSnapToLowerRemaining() {
+		assertEquals(3.5, SnapCalculator.snapToLower(3.5, 0.5), RoutingConstants.EPSILON);
+		assertEquals(3.5, SnapCalculator.snapToLower(3.5+RoutingConstants.EPSILON, 0.5), RoutingConstants.EPSILON);
+		assertEquals(3.5, SnapCalculator.snapToLower(3.5-RoutingConstants.EPSILON, 0.5), RoutingConstants.EPSILON);
+	}
+	
+	@Test
+	public void testSnapToLowerBasic() {
+		assertEquals(3.5, SnapCalculator.snapToLower(3.501, 0.5), RoutingConstants.EPSILON);
+		assertEquals(3.0, SnapCalculator.snapToLower(3.409, 0.5), RoutingConstants.EPSILON);
+	}
+	
+	@Test
+	public void testSnapToLowerLargerSnap() {
+		assertEquals(0, SnapCalculator.snapToLower(3.5, 10), RoutingConstants.EPSILON);
+		assertEquals(0, SnapCalculator.snapToLower(2*RoutingConstants.EPSILON, 10), RoutingConstants.EPSILON);
+		assertEquals(10, SnapCalculator.snapToLower(10.001, 10), RoutingConstants.EPSILON);
+	}
+
+	@Test
+	public void testSnapToLowerNegative() {
+		assertEquals(-4.0, SnapCalculator.snapToLower(-3.501, 0.5), RoutingConstants.EPSILON);
+		assertEquals(-3.5, SnapCalculator.snapToLower(-3.409, 0.5), RoutingConstants.EPSILON);
+	}	
+	
+	@Test
+	public void testSnapToLowerNegativeRemaining() {
+		assertEquals(-3.5, SnapCalculator.snapToLower(-3.5, 0.5), RoutingConstants.EPSILON);
+		assertEquals(-3.5, SnapCalculator.snapToLower(-3.5+RoutingConstants.EPSILON, 0.5), RoutingConstants.EPSILON);
+		assertEquals(-3.5, SnapCalculator.snapToLower(-3.5-RoutingConstants.EPSILON, 0.5), RoutingConstants.EPSILON);
+	}
+	
+
 }
